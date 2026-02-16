2a67cea836552c5cd3ed8dac96818969c4539543
Горшунов Арсений Юрьевич <A.Gorshunov@mkit.local>
2026-02-16 14:09:16 +0300

    Добавлено умножение и деление!!!!

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -6,10 +6,16 @@ int add(int a, int b) {
 int subtract(int a, int b) {
 	return a - b;
 }
+int mnozh(double a, double b) {
+	return a * b;
+}
+int del(double a, double b) {
+	return a / b;
+}
 int main() {
 	setlocale(LC_ALL, "ru");
 	cout << "=== КАЛЬКУЛЯТОР v1.0 === " << endl;
-	cout << "Доступные операции : +" << endl;
+	cout << "Доступные операции : +, -, *, /" << endl;
 	int a, b;
 	char op;
 	cout << " Введите выражение(например: 5 + 3) : " << endl;
@@ -19,6 +25,12 @@ int main() {
 	}
 	else if (op == '-') {
 		cout << " Результат: " << subtract(a, b) << endl;
+	}
+	else if (op == '*') {
+		cout << " Результат: " << mnozh(a, b) << endl;
+	}
+	else if (op == '/') {
+		cout << " Результат: " << del(a, b) << endl;
 	}
 	else {
 		cout << " Ошибка: неподдерживаемая операция! " << endl;
