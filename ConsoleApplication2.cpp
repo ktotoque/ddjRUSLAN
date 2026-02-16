b72a63502993ac53cd1503579d024ff0fbfc3033
Горшунов Арсений Юрьевич <A.Gorshunov@mkit.local>
2026-02-16 14:02:18 +0300

    calc2

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,6 +1,27 @@
 ﻿#include <iostream>;
 using namespace std;
-
+int add(int a, int b) {
+	return a + b;
+}
+int subtract(int a, int b) {
+	return a - b;
+}
 int main() {
-	cout << "hello world" << endl;
+	setlocale(LC_ALL, "ru");
+	cout << "=== КАЛЬКУЛЯТОР v1.0 === " << endl;
+	cout << "Доступные операции : +" << endl;
+	int a, b;
+	char op;
+	cout << " Введите выражение(например: 5 + 3) : " << endl;
+	cin >> a >> op >> b;
+	if (op == '+') {
+		cout << "Результат: " << add(a, b) << endl;
+	}
+	else if (op == '-') {
+		cout << " Результат: " << subtract(a, b) << endl;
+	}
+	else {
+		cout << " Ошибка: неподдерживаемая операция! " << endl;
+	}
+	return 0;
 }
